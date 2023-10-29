3cfc9d0799210b329ec539bf48d1ff1602c5b642
ayouzi <24679755+ayouzi@users.noreply.github.com>
2023-10-29 02:26:15 +0100

    first code ng

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"navbar",component:NavbarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
